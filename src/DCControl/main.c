4b16604dcc06d9b585dc8c0c24bd32849448d4de
Matías Moreno <matiasmoreno@uc.cl>
2025-09-01 22:09:28 -0400

    Added: fork to status

--- src/DCControl/main.c
+++ src/DCControl/main.c
@@ -43,24 +43,62 @@ int main(int argc, char const *argv[])
     // launch <executable> <arg1> <arg2> ... <argn>
     if (strcmp(input[0], "launch") == 0) {
       // Proceso hijo
-      int process = fork();
-      printf("Proceso hijo creado con PID: %d\n", process);
-      if (process == 0) { 
+      int launch_process = fork();
+      printf("Proceso hijo creado con PID: %d\n", launch_process);
+      if (launch_process == 0) { 
         printf("Ejecutando: %s\n", input[1]);
         execvp(input[1], &input[1]);
         exit(EXIT_FAILURE);
        
       } else {
         // Proceso padre
-        int index = pid_append(process, pid);
+        int index = pid_append(launch_process, pid);
       }
     }
 
     // status
     else if (strcmp(input[0], "status") == 0) {
-      sleep(5);
-      printf("Hola\n");
+      int status_process = fork();
+      if (status_process == 0) {
+        // Proceso hijo
+        printf("Estado del proceso: %d\n", getpid());
+        sleep(5);
+        printf("Hola\n");
+        exit(EXIT_SUCCESS);
+        
+      } else {
+        // Proceso padre
+        int index = pid_append(status_process, pid);
+      }
+     
     }
+
+    // abort <time>
+    else if (strcmp(input[0], "abort") == 0) {
+      if (input[1] != NULL) {
+        int time = atoi(input[1]);
+        printf("Abortando proceso en %d segundos...\n", time);
+        sleep(time);
+      } else {
+        printf("Uso: abort <time>\n");
+      }
+    }
+
+    // status
+    else if (strcmp(input[0], "status") == 0) {
+     
+    }
+
+    // status
+    else if (strcmp(input[0], "status") == 0) {
+     
+    }
+
+    // status
+    else if (strcmp(input[0], "status") == 0) {
+     
+    }
+    
 
     else {
       printf("Debug: Comando inválido\n");
